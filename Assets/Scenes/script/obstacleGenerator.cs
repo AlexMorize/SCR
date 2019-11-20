88fd885dcd64d5f5243c7d4a663645284c189f45
Anis Mezdari <mezdari.anis@gmail.com>
2019-11-20 11:59:54 +0100

    setting obstacles model

--- Assets/Scenes/script/obstacleGenerator.cs
+++ Assets/Scenes/script/obstacleGenerator.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class obstacleGenerator : MonoBehaviour
 {
 
-    public GameObject wall;
-    public GameObject hole;
-    public GameObject AI_car;
+    public List<GameObject> obstacles;
 
     public float eachSeconds = 5;
 
@@ -34,43 +32,43 @@ public class obstacleGenerator : MonoBehaviour
         // Obstacles
         if (randomNumber == 0)
         {
-            Instantiate(wall, new Vector3(-7.57f, 0.79f, 17.66f), Quaternion.identity);
+            Instantiate(obstacles[0], new Vector3(-7.57f, 0.79f, 17.66f), Quaternion.identity);
         }
         if (randomNumber == 1)
         {
-            Instantiate(wall, new Vector3(-0.44f, 0.79f, 17.66f), Quaternion.identity);
+            Instantiate(obstacles[0], new Vector3(-0.44f, 0.79f, 17.66f), Quaternion.identity);
         }
         if (randomNumber == 2)
         {
-            Instantiate(wall, new Vector3(6.69f, 0.79f, 17.66f), Quaternion.identity);
+            Instantiate(obstacles[0], new Vector3(6.69f, 0.79f, 17.66f), Quaternion.identity);
         }
         
         // hole
         if(randomNumber == 4)
         {
-            Instantiate(hole, new Vector3(-7.57f, 0.201f, 17.66f), Quaternion.identity);
+            Instantiate(obstacles[1], new Vector3(-7.57f, 0.201f, 17.66f), Quaternion.identity);
         }
         if (randomNumber == 5)
         {
-            Instantiate(hole, new Vector3(-0.44f, 0.201f, 17.66f), Quaternion.identity);
+            Instantiate(obstacles[1], new Vector3(-0.44f, 0.201f, 17.66f), Quaternion.identity);
         }
         if (randomNumber == 6)
         {
-            Instantiate(hole, new Vector3(6.69f, 0.201f, 17.66f), Quaternion.identity);        
+            Instantiate(obstacles[1], new Vector3(6.69f, 0.201f, 17.66f), Quaternion.identity);        
         }
 
         // AI car
         if (randomNumber == 7)
         {
-            Instantiate(AI_car, new Vector3(-7.57f, 0.201f, 17.66f), Quaternion.identity);
+            Instantiate(obstacles[2], new Vector3(-7.57f, 0.201f, 17.66f), Quaternion.identity);
         }
         if (randomNumber == 8)
         {
-            Instantiate(AI_car, new Vector3(-0.44f, 0.201f, 17.66f), Quaternion.identity);
+            Instantiate(obstacles[2], new Vector3(-0.44f, 0.201f, 17.66f), Quaternion.identity);
         }
         if (randomNumber == 9)
         {
-            Instantiate(AI_car, new Vector3(6.69f, 0.201f, 17.66f), Quaternion.identity);
+            Instantiate(obstacles[2], new Vector3(6.69f, 0.201f, 17.66f), Quaternion.identity);
         }
 
     }
