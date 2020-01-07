867f53a842b5ce11b8f2c839297965e1824bc86c
Alex <alexandre.morize@hotmail.fr>
2020-01-07 12:59:44 +0100

    z

---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    CarMove car;
+    public float Score;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        car = FindObjectOfType<CarMove>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score += car.vitesse * Time.deltaTime;
+    }
+}
